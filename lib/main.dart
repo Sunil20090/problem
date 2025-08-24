cc3c6074e6a8361429e509f8f3d2fffbb3ce718e
sunil <sunil.k20090@gmail.com>
2025-08-24 20:07:08 +0530

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:Problem/pages/dashboard/acount/auth/edit_profile_screen.dart';
-import 'package:Problem/pages/dashboard/acount/auth/login_page.dart';
-import 'package:Problem/pages/dashboard/dashboard_screen.dart';
 import 'package:Problem/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,6 +17,7 @@ class MyApp extends StatelessWidget {
           textTheme: GoogleFonts.montserratTextTheme(),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
+
         ),
         home: SplashScreen());
   }
